72df07ca5b48b56a978754cb2003fb80d5d9b3eb
alangerbeck <a.langerbeck@pm.me>
2025-12-03 22:10:42 +0100

    Finnished day_3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -37,11 +37,57 @@ def main():
 
     bank_sum = 0;
     
-    for battery_banks in input_list:
+    for battery_bank in input_list:
+        #print(f"Battery Bank: {''.join(battery_bank)}")
         pick_index = 0
-        start_windows_index = 12 - len(battery_banks)
-        print(start_windows_index)
+        start_window_index = -12
+        chosen_nbrs = []
+        maximum = max(battery_bank[:start_window_index])
+        
+        if maximum < battery_bank[start_window_index]:
+            temp_sum = "".join(battery_bank[start_window_index:])
+            # print(f"Added the following combination: {temp_sum}")
+            bank_sum += int(temp_sum)
+            pass
+        
+        pick_index = battery_bank.index(maximum,0,start_window_index)
+        chosen_nbrs.append(maximum)
+        #print(f"First")
+        #print(chosen_nbrs)
+        #print(f"Window Start: {start_window_index}")
+        start_window_index += 1
+        while(True):
+            #print(f"Window Start: {start_window_index}")
+            #print("Pick from")
+            #print(battery_bank[pick_index + 1:start_window_index])
+            #print("Window:")
+            #print(battery_bank[start_window_index:])
+            maximum = max(battery_bank[pick_index + 1:start_window_index])
+            #print("Chosen nbrs")
+            if maximum < battery_bank[start_window_index]:
+                for nbr in battery_bank[start_window_index:]:
+                    chosen_nbrs.append(nbr)
+                #print(chosen_nbrs)
+                break
+            chosen_nbrs.append(maximum)
+            if 12  + start_window_index - len(chosen_nbrs) == 0:
+                for nbr in battery_bank[start_window_index:]:
+                    chosen_nbrs.append(nbr)
+                #print(chosen_nbrs)
+                break
+            pick_index = battery_bank.index(maximum,pick_index +1 ,start_window_index) 
+            start_window_index += 1
+            #print(" ".join(chosen_nbrs))
+            if start_window_index == 0:
+                break
+
+        #print(" ".join(chosen_nbrs))
+        bank_sum += int("".join(chosen_nbrs))
+        
             
+
+
+
     print(f"Part 2 sum: {bank_sum}")
 
 if __name__ == '__main__':
