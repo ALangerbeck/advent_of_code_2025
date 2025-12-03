118d5d4ac602fdce63c5f66c6a4f919f918f5ddf
Alfred Langerbeck <Alfred.Langerbeck@advenica.com>
2025-12-03 17:06:00 +0100

    Started formulating thoughts day3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,7 +1,5 @@
 import sys
-
-
-
+   
 def main():
     input_list = []
     # Read input line by line
@@ -9,27 +7,42 @@ def main():
         if line.strip() == "":
             pass
         else:
-            #input_list.append(line.split())
-            sublist = []
-            for i,item in enumerate(line.strip()):
-             sublist.append((item,i))
-            input_list.append(sublist)
+            input_list.append(list(line.strip()))
+            #sublist = []
+            #for i,item in enumerate(line.strip()):
+            # sublist.append((item,i))
+            #input_list.append(sublist)
     #print(input_list)
     bank_sum = 0;
-
+    
     for battery_banks in input_list:
-        battery_sorted = sorted(battery_banks, key=lambda x: x[0], reverse=True)
-        if battery_sorted[0][1] < battery_sorted[1][1]:
-            temp_sum = int(battery_sorted[0][0] + battery_sorted[1][0])
-            print(f"Sum is {temp_sum}")
-            bank_sum += temp_sum
-        else:
-            temp_sum = int(battery_sorted[1][0] + battery_sorted[0][0])
-            print(f"Sum is {temp_sum}")
-            bank_sum += temp_sum
+        battery_sorted = sorted(battery_banks[:-1],reverse=True)
+        #print(battery_sorted)
+        first = battery_sorted[0]
+        battery_sorted = sorted(battery_banks[battery_banks.index(first)+1:],reverse=True)
+        #print(battery_sorted)
+        temp_sum = int(str(first) + str(battery_sorted[0]))
+        #print(temp_sum)
+        bank_sum += temp_sum
+        #if battery_sorted[0][1] < battery_sorted[1][1]:
+        #    temp_sum = int(battery_sorted[0][0] + battery_sorted[1][0])
+        #    print(f"Sum is {temp_sum}")
+        #    bank_sum += temp_sum
+        #else:
+        #    temp_sum = int(battery_sorted[1][0] + battery_sorted[0][0])
+        #    print(f"Sum is {temp_sum}")
+        #    bank_sum += temp_sum
 
     print(f"Part 1 sum: {bank_sum}")
 
+    bank_sum = 0;
+    
+    for battery_banks in input_list:
+        pick_index = 0
+        start_windows_index = 12 - len(battery_banks)
+        print(start_windows_index)
+            
+    print(f"Part 2 sum: {bank_sum}")
 
 if __name__ == '__main__':
     main()
